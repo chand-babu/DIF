83657740d2b789459671395d459e12a87413a2cd
Rohit Kumar S <rkd35109@hotmail.com>
2020-04-28 16:23:26 +0530

    blog-comment-implemetation-doing

--- mvc/action/blog/commentAction.php
+++ mvc/action/blog/commentAction.php
@@ -5,12 +5,17 @@ $comment = new \controller\blog\BlogController();
 
 $data = array(
     'blg_id' => $_POST['blog_id'],
-    'com_id' => 'COM'. md5(uniqid(rand(), true)),
-    'name' => $_POST['comment_name'],
-    'message' => $_POST['comment_msg'],
-    'com_status' => 0,
-    'created' => date("Y-m-d h:i:sa")
+    'data' => json_encode([array(
+        'com_id' => 'COM'. md5(uniqid(rand(), true)),
+        'name' => $_POST['comment_name'],
+        'message' => $_POST['comment_msg'],
+        'reply' => [],
+        'com_status' => 0,
+        'created' => date("Y-m-d h:i:sa")
+        )]
+    )
 );
-print_r(json_encode([$data]));
-$response = $comment->commentBlogController([$data]);
-// echo json_encode($response);
+//print_r(json_encode([$data]));
+$response = $comment->commentBlogController($data);
+//print_r($response);
+echo json_encode($response);
--- mvc/model/blog/BlogModel.php
+++ mvc/model/blog/BlogModel.php
@@ -77,6 +77,57 @@ class BlogModel extends DatabaseService{
             );
         }
     }
+
+    public function commentBlogModel($input) {
+        //print_r($input[0]['blg_id']);
+        $query = "SELECT comment FROM blog WHERE blg_id = :blg_id";
+        try {
+            $execute = $this->connection->prepare($query);
+            $execute->execute(array('blg_id' => $input['blg_id']));
+            $data = $execute->fetch(\PDO::FETCH_ASSOC);
+            if (empty($data['comment'])) {
+                return $this->commentAddBlogModel($input);
+            } else {
+                $oldData = json_decode($data['comment'], true);
+                array_push($oldData, json_decode($input['data'], true)[0]);
+                $pushComment = array(
+                    'blg_id' => $input['blg_id'],
+                    'data' => json_encode($oldData)
+                );
+                return $this->commentAddBlogModel($pushComment);
+                //$this->commentAddBlogModel($pushComment);
+            }
+        } catch (\PDOException $e) {
+            return array(
+                'result' => false,
+                'message' => 'Something went wrong',
+                'data' => [],
+                'dev' => $e->getMessage()
+            );
+        }
+    }
+
+    public function commentAddBlogModel($input) {
+        //print_r($input);
+        $query = "UPDATE blog SET comment = :data WHERE blg_id = :blg_id";
+        try {
+            $execute = $this->connection->prepare($query);
+            $execute->execute($input);
+            return array(
+                'result' => true,
+                'message' => 'DATA Added',
+                'dev' => ''
+            );
+        } catch (\PDOException $e) {
+            print_r($e->getMessage());
+            return array(
+                'result' => false,
+                'message' => 'Something went wrong',
+                'data' => [],
+                'dev' => $e->getMessage()
+            );
+        }
+    }
 }
 
 
